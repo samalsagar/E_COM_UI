85ad03f6b59aec9fcacaab27eecf652af63f8f1b
samalsagar <sagar.samal6264@gmail.com>
2025-12-04 22:59:53 +0530

    Corretced

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navigation from './components/Navigation/Navigation'
+import Navigation from './components/Navigation/navigation'
 import HeroSection from './components/HeroSection/HeroSection'
 
 function Shop() {
