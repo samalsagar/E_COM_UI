3387c94d74a564bf8177a8581ab7f64648936545
samalsagar <sagar.samal6264@gmail.com>
2025-12-04 23:08:01 +0530

    Build fail

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navigation from './components/Navigation/navigation'
+import Navigation from './components/Navigation/NavBar'
 import HeroSection from './components/HeroSection/HeroSection'
 
 function Shop() {
